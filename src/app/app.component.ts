53fb4d328d41121e7c4bdac50770eea3ae00839b
Akshay Kumbhare <52664007+Akshay7-max@users.noreply.github.com>
2022-10-05 17:59:52 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from "./employee";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project2';
+
+  employees: Employee[] = [
+    new Employee(1, "Ram", 4000),
+    new Employee(2, "Shyam", 5000),
+    new Employee(3, "Krishna", 6000),
+    new Employee(4, "Hari", 7000)
+  ];
+
+  newemployee:Employee = new Employee(null, null, null);
+
+  onInsertClick() {
+    this.employees.push(new Employee(this.newemployee.empid, this.newemployee.empname, this.newemployee.salary));
+
+    this.newemployee.empid = null;
+    this.newemployee.empname = null;
+    this.newemployee.salary = null;
+  }
+
+  onDeleteClick(n:any) {
+    if (confirm("Are you sure to delete this employee?")) {
+      this.employees.splice(n, 1);
+    }
+  }
 }
